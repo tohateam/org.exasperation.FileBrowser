e70887be9571ff52c3e48faa1870de9a35f938b2
Junseok Lee <flyingyellow@gmail.com>
2012-04-08 17:52:44 -0700

    switched to custom adapter + display of date

--- src/org/exasperation/FileManager/FileBrowser.java
+++ src/org/exasperation/FileManager/FileBrowser.java
@@ -1,10 +1,16 @@
 package org.exasperation.FileManager;
 
+import java.util.Date;
+import java.text.SimpleDateFormat;
 import java.io.File;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import android.widget.TextView;
+import android.content.Context;
+import android.view.ViewGroup;
+import android.view.LayoutInflater;
 import android.widget.ListView;
 import android.view.View;
 import android.app.AlertDialog;
@@ -20,10 +26,11 @@ public class FileBrowser extends ListActivity
     public static final String TAG = "org.exasperation.FileManager";
     public static final String DIR_DIVIDER = "/";
     public static final String ROOT_DIR = "/";
+    public static final String DATE_FORMAT = "MMM d, yyyy";
     
     String homeDirectory = "/sdcard/";
     File currentDirectory = new File(homeDirectory);
-    List<String> directoryEntries = new ArrayList<String>();
+    List<File> directoryEntries = new ArrayList<File>();
 
     /** Called when the activity is first created. */
     @Override
@@ -39,7 +46,7 @@ public class FileBrowser extends ListActivity
         if (aDirectory.isDirectory())
         {
             currentDirectory = aDirectory;
-            String[] fileList = currentDirectory.list();
+            File[] fileList = currentDirectory.listFiles();
             Arrays.sort(fileList);
             fill(fileList);
         }
@@ -68,27 +75,51 @@ public class FileBrowser extends ListActivity
         browseTo(new File(ROOT_DIR));
     }
 
-	private void fill(String[] files) {
+	private void fill(File[] files) {
 		this.directoryEntries.clear();
-        if (currentDirectory.getParentFile() != null )
-            directoryEntries.add(getString(R.string.up_one_level));
-
-	    for (String file : files){
+	    for (File file : files){
 	        this.directoryEntries.add(file);
 	    }
 
-        ArrayAdapter<String> directoryList = new ArrayAdapter<String>(this, android.R.layout.simple_selectable_list_item, this.directoryEntries);
-        setListAdapter(directoryList);
+        setListAdapter(new FileAdapter(this, R.layout.file_row, this.directoryEntries));
         
     }
 
     public void onListItemClick(ListView list, View view, int position, long id)
     {
         String newPath = null;
-        if (directoryEntries.get(position) == getString(R.string.up_one_level))
-            newPath = currentDirectory.getParent();
-        else
-            newPath = currentDirectory.getAbsolutePath() + DIR_DIVIDER + directoryEntries.get(position);
+        newPath = currentDirectory.getAbsolutePath() + DIR_DIVIDER + directoryEntries.get(position).getName();
         browseTo(new File(newPath));
+    }
+
+
+    private class FileAdapter extends ArrayAdapter<File> {
+        private List<File> files;
+        public FileAdapter(Context context, int textViewResourceId, List<File> files) {
+            super(context, textViewResourceId, files);
+            this.files = files;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View v = convertView;
+            if (v == null) {
+                LayoutInflater vi = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                v = vi.inflate(R.layout.file_row, null);
+            }
+            File o = files.get(position);
+            if (o != null) {
+                TextView fileName = (TextView) v.findViewById(R.id.file_name);
+                TextView fileMeta = (TextView) v.findViewById(R.id.file_meta);
+                if (fileName != null) {
+                    fileName.setText(o.getName());
+                }
+                if( fileMeta != null){
+                    SimpleDateFormat format = new SimpleDateFormat(DATE_FORMAT);
+                    fileMeta.setText(format.format(new Date(o.lastModified())));
+                }
+            }
+            return v;
+        }
     }
 }
